b5a6709e1237b32e16ed1c6a2b143f682e7b15ad
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-05-23 20:17:40 -0600

    [ADD] creacion de archivo finalizado

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const requestPromise = require('request-promise');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const {Parser} = require('json2csv');
+const XLSX = require('xlsx');
 
 let paginacionArray = [];
 let resultadosArray = [];
+let resulatdostoExcle = [];
 
 (async () => {
     try {
         let response = await requestPromise("https://quotes.toscrape.com/");
         let $ = cheerio.load(response); // html
 
-
-        // const obtenerPaginaciones = $('ul.pager > li > a').attr('href');
 
         paginacionArray.push(`https://quotes.toscrape.com/`);
 
@@ -35,18 +37,73 @@ let resultadosArray = [];
 
         // recuperar datos
         for(let url of paginacionArray){
-            let response = await requestPromise(urlVisitada);
+            let response = await requestPromise(url);
             let $ = cheerio.load(response);
 
-            let frase = $('');
+            const results = []
+
+
+            $('div.row').eq(1).find('div.quote').each(function() {
+                let frase = $(this).find('span.text').text();
+                let author = $(this).find('span > small.author').text();
+
+                let tags = [];
+
+                $(this).find('div.tags > a').each(function() {
+                    let tag = $(this).text();
+                    tags.push(tag);
+                })
+                // console.log(frase);
+                // console.log(author);
+
+                const resultadosBusqueda = {
+                    quote: frase,
+                    author: author,
+                    tags
+                }
+
+                results.push(resultadosBusqueda);
+            })
+            // console.log(results);
+
+            resultadosArray.push(...results);
 
         }
 
+        //Crear archivo JSON
+        let data = JSON.stringify(resultadosArray);
+        fs.writeFileSync("quotes.json", data);
+        console.log("ARCVHIVO JSON CREADO!!!");
 
-        // // Obtener paginaciones
-        // for(let i = 1; i < 10; i++){
-        //     paginacionArray.push(`https://quotes.toscrape.com/${page}/${i}/`);
-        // }
+        //Crear archivo CSV
+        const fields = ['quote', 'author', 'tags'];
+        const json2csvParse =  new Parser({
+            fields: fields,
+            defaultValue: 'No Hay Info',
+        });
+        const csv = json2csvParse.parse(resultadosArray);
+        fs.writeFileSync("uotes.csv", csv, "utf-8");
+        console.log("ARCVHIVO CSV CREADO!!!");
+ 
+        //Crear archivo XLSX
+        resultadosArray.forEach(item => {
+            const data = {
+                quote: item.quote,
+                author: item.author,
+                tags: item.tags.join(', ') 
+            }
+
+            resulatdostoExcle.push(data);
+        })
+
+        const worksheet = XLSX.utils.json_to_sheet(resulatdostoExcle);
+
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Quotes');
+
+        XLSX.writeFile(workbook, 'quotes.xlsx');
+        console.log('ARCHIVO XLSX CREADO!!');
+
 
     } catch (error) {
         console.log(error);
